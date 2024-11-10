c25b535eeb74acb52b308c3ff00c06f611666859
Nicola Venturi <n.venturi3@studenti.unipi.it>
2024-11-10 17:48:28 +0100

    Init DB enanched with tables creation

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -1,5 +1,37 @@
-CREATE DATABASE IF NOT EXISTS `authentication`;
-CREATE DATABASE IF NOT EXISTS `novels`;
-GRANT ALL ON `authentication`.* TO 'admin'@'%';
-GRANT ALL ON `novels`.* TO 'admin'@'%';
+CREATE DATABASE IF NOT EXISTS `authentication_db`;
+CREATE DATABASE IF NOT EXISTS `novels_db`;
+GRANT ALL ON `authentication_db`.* TO 'admin'@'%';
+GRANT ALL ON `novels_db`.* TO 'admin'@'%';
 
+USE `authentication_db`;
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(50) NOT NULL UNIQUE,
+    `password_hash` VARCHAR(255) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+USE `novels_db`;
+
+-- Application related users information
+CREATE TABLE IF NOT EXISTS `user_profiles` (
+    `user_id` INT PRIMARY KEY,                  -- Link with `authentication_db.users`
+    `is_premium` BOOLEAN DEFAULT FALSE,
+    `email` VARCHAR(100),
+    `full_name` VARCHAR(100),
+    FOREIGN KEY (`user_id`) REFERENCES `authentication_db`.`users`(`id`) ON DELETE CASCADE
+);
+
+-- Novels information
+CREATE TABLE IF NOT EXISTS `novels` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `title` VARCHAR(255) NOT NULL,
+    `description` TEXT,
+    `genre` VARCHAR(100),
+    `type` ENUM('short_story', 'full_novel') NOT NULL,
+    `file_path` VARCHAR(255),
+    `is_premium` BOOLEAN DEFAULT FALSE,
+    `uploaded_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `user_id` INT,
+    FOREIGN KEY (`user_id`) REFERENCES `user_profiles`(`user_id`) ON DELETE CASCADE
+);
