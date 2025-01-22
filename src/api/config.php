9c60d3f0b50cdefd881eb4510c15e86c87c734d9
Nicola Venturi <n.venturi3@studenti.unipi.it>
2025-01-22 16:28:08 +0100

    Added configuration to send email

---
+++ src/api/config.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'smtp_host' => getenv('SMTP_HOST'),
+    'smtp_user' => getenv('SMTP_USER'),
+    'smtp_password' => getenv('SMTP_PASSWORD'),
+    'smtp_port' => getenv('SMTP_PORT') ?: 587,
+    'smtp_encryption' => getenv('SMTP_ENCRYPTION') ?: 'tls',
+];
+?>
