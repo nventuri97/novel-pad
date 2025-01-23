2515083861488e2b446a3f14e36845d1d80ce430
Nicola Venturi <n.venturi3@studenti.unipi.it>
2025-01-23 16:03:04 +0100

    Added configuration required by mail-client

--- src/api/config.php
+++ src/api/config.php
@@ -5,5 +5,6 @@ return [
     'smtp_password' => getenv('SMTP_PASSWORD'),
     'smtp_port' => getenv('SMTP_PORT') ?: 587,
     'smtp_encryption' => getenv('SMTP_ENCRYPTION') ?: 'tls',
+    'novelpad_url' => getenv('NOVELPAD_URL'),
 ];
 ?>
