69f29658441a4e6663b155f86c98268318cbb3d8
Nicola Venturi <n.venturi3@studenti.unipi.it>
2025-01-23 16:03:24 +0100

    Added verification token to verify user

--- init/init.sql
+++ init/init.sql
@@ -8,6 +8,8 @@ CREATE TABLE IF NOT EXISTS `users` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `username` VARCHAR(50) NOT NULL UNIQUE,
     `password_hash` VARCHAR(255) NOT NULL,
+    `verification_token` VARCHAR(255) UNIQUE,
+    `is_verified` BOOLEAN DEFAULT FALSE,
     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
