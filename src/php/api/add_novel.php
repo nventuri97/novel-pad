ffdf1ce63f3cfe1f964bdad0916b5a1a5e50caf1
Nicola Venturi <n.venturi3@studenti.unipi.it>
2025-02-03 10:48:41 +0100

    Added logging configuration

--- src/php/api/add_novel.php
+++ src/php/api/add_novel.php
@@ -8,6 +8,7 @@ include '../utils/user.php';
 include '../utils/db-client.php';
 
 ob_start();
+openlog("add_novel.php", LOG_PID | LOG_PERROR, LOG_LOCAL0);
 
 $novel_conn = db_client::get_connection("novels_db");
 
@@ -19,11 +20,15 @@ $response = [
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     session_start();
     if (!isset($_SESSION["user"])) {
+        syslog(LOG_ERR, $_SERVER["REMOTE_ADDR"]." - User try to add novel without log in.");
+
         $response["message"] = "User is not logged in.";
         echo json_encode($response);
         ob_end_flush();
         exit;
     }
+
+    syslog(LOG_INFO, $_SERVER["REMOTE_ADDR"]." - User is trying to add a novel.");
 
     // Get the user ID from session
     $user_id = $_SESSION["user"]->get_id();
@@ -42,6 +47,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         // Define allowed file types and size limits
         $allowedTypes = ['application/pdf', 'text/html'];
         if (!in_array($file['type'], $allowedTypes)) {
+            syslog(LOG_ERR, $_SERVER["REMOTE_ADDR"]." - Invalid file type.");
+
             $response["message"] = "Invalid file type.";
             echo json_encode($response);
             ob_end_flush();
@@ -49,6 +56,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         }
 
         if(($type == 'full_novel' && $file['type'] != 'application/pdf')) {
+            syslog(LOG_ERR, $_SERVER["REMOTE_ADDR"]." - This type of file is not allowed.");
+
             $response["message"] = "This type of file is not allowed. Pleas upload a pdf.";
             echo json_encode($response);
             ob_end_flush();
@@ -56,6 +65,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         }
 
         if(($type == 'short_story' && $file['type'] != 'text/html')) {
+            syslog(LOG_ERR, $_SERVER["REMOTE_ADDR"]." - This type of file is not allowed.");
+
             $response["message"] = "Error occured: not an html file.";
             echo json_encode($response);
             ob_end_flush();
@@ -66,6 +77,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $uploadDir = '/var/www/private/uploads/'. $_SESSION["user"]->get_username() . '/';
         if (!is_dir($uploadDir)) {
             if (!mkdir($uploadDir, 0755, true)) {
+                syslog(LOG_ERR, $_SERVER["REMOTE_ADDR"]." - Failed to create upload directory.");
+
                 $response["message"] = "Failed to create upload directory.";
                 echo json_encode($response);
                 ob_end_flush();
@@ -76,6 +89,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         // Save file to a directory (ensure appropriate directory exists and has write permissions)
         $filePath = $uploadDir . basename($file['name']);
         if (!move_uploaded_file($file['tmp_name'], $filePath)) {
+            syslog(LOG_ERR, $_SERVER["REMOTE_ADDR"]." - Failed to upload the file.");
+
             $response["message"] = "Failed to upload the file." . $file['tmp_name'] . " and " . $filePath;
             echo json_encode($response);
             ob_end_flush();
@@ -102,6 +117,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
         $stmt->execute();
 
+        syslog(LOG_INFO, $_SERVER["REMOTE_ADDR"]." - Novel added successfully");
+
         $response["success"] = true;
         $response["message"] = "Novel added successfully!";
     } catch (Exception $e) {
@@ -109,6 +126,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
 }
 
+closelog();
 echo json_encode($response);
 ob_end_flush();
 exit;
