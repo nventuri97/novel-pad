e3c34eb4b54250225adbfd359f4491e0d139ca39
Nicola Venturi <n.venturi3@studenti.unipi.it>
2025-02-12 13:44:30 +0100

    Bug fix in password validation regex

--- src/js/register.js
+++ src/js/register.js
@@ -40,14 +40,14 @@ document.getElementById('registerForm').addEventListener('submit', function(even
         return;
     }
     
-    let passwordRegex='/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/';
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
     if (password.length < 8) {
         errorMessage.textContent = "Password must be at least 8 characters.";
         errorMessage.style.display = 'block';
         document.getElementById('password').focus();
         grecaptcha.reset();
         return;
-    } else if (!password.match(passwordRegex)) {
+    } else if (!passwordRegex.test(password)) {
         errorMessage.textContent = "Password must agree password policy";
         errorMessage.style.display = 'block';
         document.getElementById('password').focus();
