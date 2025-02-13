a63de8add52299bbaa69c6831484eda691804d29
leonardo@cyborg <leomanne2000@gmail.com>
2025-02-13 10:34:37 +0100

    fixed stuff on db

--- init/init.sql
+++ init/init.sql
@@ -23,8 +23,6 @@ CREATE TABLE IF NOT EXISTS `admins` (
     `name` VARCHAR(50) UNIQUE NOT NULL,
     `email` VARCHAR(100) UNIQUE NOT NULL,
     `is_verified` BOOLEAN DEFAULT FALSE,
-    `reset_token` VARCHAR(255) UNIQUE,
-    `reset_token_expiry` TIMESTAMP,
     `logged_in` BOOLEAN DEFAULT FALSE,
     `password_hash` VARCHAR(255) NOT NULL,
     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
