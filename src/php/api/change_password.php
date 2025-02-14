5229c446c5a5ec4e2966abb5f3f64b2dfe8e2128
francesco@cyborg <francesco.beno00gmail.com>
2025-02-14 17:57:56 +0100

    add input check in change_password

--- src/php/api/change_password.php
+++ src/php/api/change_password.php
@@ -5,11 +5,11 @@ header('Content-Type: application/json');
 use ZxcvbnPhp\Zxcvbn;
 
 require __DIR__ . '/../utils/user.php';
-require_once __DIR__ . '/../utils/db-client.php';
+require __DIR__ . '/../utils/db-client.php';
 require __DIR__.'/../../vendor/autoload.php';
 
 // Load the config if necessary
-$config = require_once __DIR__ . '/../utils/config.php';
+$config = require __DIR__ . '/../utils/config.php';
 session_start();
 ob_start();
 openlog("change_password.php", LOG_PID | LOG_PERROR, LOG_LOCAL0);
@@ -20,118 +20,138 @@ $response = [
     "message" => ""
 ];
 
-// Verify that the admin is logged in and needs to force password change
-if (!isset($_SESSION["user"])) {
-    http_response_code(403);
-    echo json_encode([
-        "success" => false,
-        "message" => "Not authorized or password change not required."
-    ]);
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    syslog(LOG_ERR, $_SERVER["REMOTE_ADDR"]. " - - [" . date("Y-m-d H:i:s") . "]  Invalid request method");
+
+    http_response_code(405); // HTTP method not allowed
+    header("Content-Type: text/html");
+
+    echo "<h1>405 Method Not Allowed</h1>";
+    echo "<p>The request method is not allowed. This method is not allowed.</p>";
     exit;
 }
 
-try {
-    if ($_SERVER["REQUEST_METHOD"] === "POST") {
-        $currentPassword = $_POST["currentPassword"] ?? '';
-        $newPassword = $_POST["newPassword"] ?? '';
+if (!isset($_SESSION['user'])) {
+    syslog(LOG_ERR, $_SERVER["REMOTE_ADDR"]." - - [" . date("Y-m-d H:i:s") . "] User not authenticated.");
 
-        if (empty($currentPassword) || empty($newPassword)) {
-            syslog(LOG_ERR, $_SERVER['REMOTE_ADDR'] . ' - - [' . date("Y-m-d H:i:s") . ']  Attempt to change password with current password or new password empty');
-            
-            http_response_code(400);
-            $response["message"] = "New password is required.";
-            echo json_encode($response);
-            exit;
-        }
+    session_destroy();
+    http_response_code(401); // Unauthorized
+    header("Content-Type: text/html");
 
-        // Server side password validation
-        // Password must be at least 8 characters long
-        if (strlen($newPassword) < 8) {
-            syslog(LOG_ERR, $_SERVER['REMOTE_ADDR'] . ' - - [' . date("Y-m-d H:i:s") . ']  Password too short.');
+    echo "<h1>401 User not authenticated</h1>";
+    echo "<p>The user is not authorized.</p>";
+    exit;
+}
 
-            $response['message'] = "Password must be at least 8 characters long.";
-            echo json_encode($response);
-            ob_end_flush();
-            exit;
-        }
+if(!isset($_SESSION["timeout"]) || $_SESSION["timeout"] < date("Y-m-d H:i:s")) {
+    syslog(LOG_ERR, $_SERVER["REMOTE_ADDR"]." - - [" . date("Y-m-d H:i:s") . "] Session expired.");
 
-        // Password must contain at least one uppercase letter, one lowercase letter, one number, and no special characters
-        $password_regex='/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/';
-        if (preg_match($password_regex, $newPassword)){
-            syslog(LOG_ERR, $_SERVER['REMOTE_ADDR'] . ' - - [' . date("Y-m-d H:i:s") . ']  Password too weak.');
+    session_destroy();
+    http_response_code(419); // Timeout error
+    header("Content-Type: text/html");
 
-            $response['message'] = "Password must agree password policy.";
-            echo json_encode($response);
-            ob_end_flush();
-            exit;
-        }
+    echo "<h1>419 Session expired</h1>";
+    echo "<p>The user session is expired, try to login again.</p>";
+    exit;
+}
 
-        // Check password strength using zxcvbn
-        $zxcvbn = new Zxcvbn();
-        $result = $zxcvbn->passwordStrength($newPassword, $userInputs = [$_SESSION["user"]->get_email(), $_SESSION["user"]->get_nickname()]);
-        if ($result['score']<4){
-            syslog(LOG_ERR, $_SERVER['REMOTE_ADDR'] . ' - - [' . date("Y-m-d H:i:s") . ']  Password too weak.');
+$currentPassword = $_POST["currentPassword"] ?? '';
+$newPassword = $_POST["newPassword"] ?? '';
 
-            $response['message'] = "Password too weak.";
-            echo json_encode($response);
-            ob_end_flush();
-            exit;
-        }
-
-        if ($currentPassword === $newPassword){
-            syslog(LOG_ERR, $_SERVER['REMOTE_ADDR'] . ' - - [' . date("Y-m-d H:i:s") . ']  New password corresponds to current one.');
-
-            $response['message'] = "New password must be different from the current one";
-            echo json_encode($response);
-            ob_end_flush();
-            exit;
-        }
-
-        // Get the database connection for authentication
-        $auth_conn = db_client::get_connection("authentication_db");
-        $user_id=$_SESSION["user"]->get_id();
-
-        $stmt = $auth_conn->prepare("SELECT password_hash FROM users WHERE id = :id");
-        $stmt->bindParam(':id', $user_id);
-        $stmt->execute();
-        $storedPassword = $stmt->fetchColumn();
-
-        if (!$storedPassword || !password_verify($currentPassword, $storedPassword)) {
-            syslog(LOG_ERR, $_SERVER['REMOTE_ADDR'] . ' - - [' . date("Y-m-d H:i:s") . ']  Incorrect password.');
-
-            $response["message"] = "Incorrect password.";
-            echo json_encode($response);
-            ob_end_flush();
-            exit;
-        }
-
-        // Calculate the hash of the new password (using BCRYPT)
-        $newPasswordHash = password_hash($newPassword, PASSWORD_BCRYPT);
-
-        // Update the password and set is_verified to true
-        $stmt = $auth_conn->prepare("
-            UPDATE users
-            SET password_hash = :password_hash
-            WHERE id = :id
-        ");
-        $stmt->bindParam(':password_hash', $newPasswordHash);
-        $stmt->bindParam(':id', $_SESSION["user"]->get_id(), PDO::PARAM_INT);
-        $stmt->execute();
-
-        $response["success"] = true;
-        $response["message"] = "Password changed successfully.";
-        
-    } else {
-        http_response_code(405);
-        $response["message"] = "Method not allowed.";
-        echo json_encode($response);
-        exit;
-    }
-} catch (PDOException $e) {
-    http_response_code(500);
-    $response["message"] = "Database error: " . $e->getMessage();
+if (empty($currentPassword) || empty($newPassword)) {
+    syslog(LOG_ERR, $_SERVER['REMOTE_ADDR'] . ' - - [' . date("Y-m-d H:i:s") . ']  Attempt to change password with current password or new password empty');
+    
+    http_response_code(400);
+    $response["message"] = "New password is required.";
     echo json_encode($response);
     exit;
+}
+
+// Server side password validation
+// Password must be at least 8 characters long
+if (strlen($newPassword) < 8) {
+    syslog(LOG_ERR, $_SERVER['REMOTE_ADDR'] . ' - - [' . date("Y-m-d H:i:s") . ']  Password too short.');
+
+    $response['message'] = "Password must be at least 8 characters long.";
+    echo json_encode($response);
+    ob_end_flush();
+    exit;
+}
+
+// Password must contain at least one uppercase letter, one lowercase letter, one number, and no special characters
+$password_regex='/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/';
+if (preg_match($password_regex, $newPassword)){
+    syslog(LOG_ERR, $_SERVER['REMOTE_ADDR'] . ' - - [' . date("Y-m-d H:i:s") . ']  Password too weak.');
+
+    $response['message'] = "Password must agree password policy.";
+    echo json_encode($response);
+    ob_end_flush();
+    exit;
+}
+
+// Check password strength using zxcvbn
+$zxcvbn = new Zxcvbn();
+$result = $zxcvbn->passwordStrength($newPassword, $userInputs = [$_SESSION["user"]->get_email(), $_SESSION["user"]->get_nickname()]);
+if ($result['score']<4){
+    syslog(LOG_ERR, $_SERVER['REMOTE_ADDR'] . ' - - [' . date("Y-m-d H:i:s") . ']  Password too weak.');
+
+    $response['message'] = "Password too weak.";
+    echo json_encode($response);
+    ob_end_flush();
+    exit;
+}
+
+if ($currentPassword === $newPassword){
+    syslog(LOG_ERR, $_SERVER['REMOTE_ADDR'] . ' - - [' . date("Y-m-d H:i:s") . ']  New password corresponds to current one.');
+
+    $response['message'] = "New password must be different from the current one";
+    echo json_encode($response);
+    ob_end_flush();
+    exit;
+}
+
+// Update the session timeout
+$_SESSION["timeout"] = date("Y-m-d H:i:s", strtotime('+30 minutes'));
+
+try {
+    // Get the database connection for authentication
+    $auth_conn = db_client::get_connection("authentication_db");
+    $user_id=$_SESSION["user"]->get_id();
+
+    $stmt = $auth_conn->prepare("SELECT password_hash FROM users WHERE id = :id");
+    $stmt->bindParam(':id', $user_id);
+    $stmt->execute();
+    $storedPassword = $stmt->fetchColumn();
+
+    if (!$storedPassword || !password_verify($currentPassword, $storedPassword)) {
+        syslog(LOG_ERR, $_SERVER['REMOTE_ADDR'] . ' - - [' . date("Y-m-d H:i:s") . ']  Incorrect password.');
+
+        $response["message"] = "Incorrect password.";
+        echo json_encode($response);
+        ob_end_flush();
+        exit;
+    }
+
+    // Calculate the hash of the new password (using BCRYPT)
+    $newPasswordHash = password_hash($newPassword, PASSWORD_BCRYPT);
+
+    // Update the password and set is_verified to true
+    $stmt = $auth_conn->prepare("
+        UPDATE users
+        SET password_hash = :password_hash
+        WHERE id = :id
+    ");
+    $stmt->bindParam(':password_hash', $newPasswordHash);
+    $stmt->bindParam(':id', $_SESSION["user"]->get_id(), PDO::PARAM_INT);
+    $stmt->execute();
+
+    $response["success"] = true;
+    $response["message"] = "Password changed successfully.";
+} catch (PDOException $e) {
+    syslog(LOG_ERR, $_SERVER["REMOTE_ADDR"]." - - [" . date("Y-m-d H:i:s") . "] An error occurred while processing user data. ".$e->getMessage());
+
+    http_response_code(500); // Internal Server Error
+    $response['message'] = 'An error occurred while processing user data.';
 }
 
 ob_end_clean();
