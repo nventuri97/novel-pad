59b7e025fdf7cdc91c45cbee830f5a15be5579c4
francesco@cyborg <francesco.beno00gmail.com>
2025-02-17 13:39:28 +0100

    add csp header and fix back-button

--- src/php/api/password_recover.php
+++ src/php/api/password_recover.php
@@ -1,4 +1,5 @@
 <?php
+header("Content-Security-Policy: default-src 'self'; script-src 'self' https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/; style-src 'self' 'unsafe-inline'; frame-src 'self' https://www.google.com/recaptcha/");
 header('Content-Type: application/json'); // Ensure the response is JSON
 
 include '../utils/db-client.php';
