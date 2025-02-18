444a9701c31230180f1d6a285507e89ba81ba916
francesco@cyborg <francesco.beno00gmail.com>
2025-02-18 19:43:15 +0100

    bug fix: another user already logged

--- src/php/api/login.php
+++ src/php/api/login.php
@@ -95,6 +95,15 @@ try{
     $stmt->execute();
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
+    if (isset($_SESSION['user']) && $_SESSION['user']->get_email() !== $email) {
+        syslog(LOG_ERR, $_SERVER["REMOTE_ADDR"]." - - [" . date("Y-m-d H:i:s") . "] User already authenticated.");
+    
+        $response["message"]= "There is a user already logged, go to dashboard.";
+        echo json_encode($response);
+        ob_end_flush();
+        exit;
+    }
+
     if (!$user) {
         syslog(LOG_ERR, $_SERVER["REMOTE_ADDR"]. " - - [" . date("Y-m-d H:i:s") . "]  User inserted wrong email");
 
